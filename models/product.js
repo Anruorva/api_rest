b9f9888b36481b33b83aee346808032f9ffc4bd0
Anruo <Anruorva@gmail.com>
2018-12-10 23:21:13 -0600

    comit
    all
    
    Co-Authored-By: isaiguti <isaiguti@users.noreply.github.com>
    Co-Authored-By: Guty1996 <guty1996@users.noreply.github.com>
    Co-Authored-By: TomasPB09 <tomaspb09@users.noreply.github.com>
    Co-Authored-By: gmofierro <gmofierro@users.noreply.github.com>

--- models/product.js
+++ models/product.js
@@ -3,13 +3,12 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-
 const ProductSchema = Schema({
   name: String,
-  photo: String,
-  price: {type:Number,defaults: 0},
-  category: {type:String , enum: ['computers','phones','accesories']},
-  description:String
+  picture: String,
+  price: { type: Number, default: 0 },
+  category: { type: String, enum: ['computers', 'phones', 'accesories'] },
+  description: String
 })
 
- module.exports = mongoose.model('Product',ProductSchema)
+module.exports = mongoose.model('Product', ProductSchema)
